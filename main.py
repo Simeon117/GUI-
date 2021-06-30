00790e745a2bdd395cb87a9cf0f100dff95c6ddb
SimeonPres <84358121+Simeon117@users.noreply.github.com>
2021-06-30 03:05:42 +0000

    Component 6 Leaderboard def function was not in the end class. Missing syntax on he end of self.end_screen.

--- main.py
+++ main.py
@@ -2,83 +2,8 @@ from tkinter import *
 import random
 
 names = []
-global questions_answers
 asked = []
 score = 0
-
-#Dictionary has key of number (for each question number) and : the value for each is a list that has 7 items, so index 0 to 6
-questions_answers = {
-  1: ["Lorem ipsum dolor sit amet, consectetur adipiscing?", #item 1, index 0 will be the question
-  'Lorem ipsum dolor sit amet consectetur adipiscing1', # Item 2, index 1 will be the first choice
-  'Lorem ipsum dolor sit amet, consectetur adipiscing2', # Item 3, index 2 will be the second choice
-  'Lorem ipsum dolor sit amet, consectetur adipiscing3', # Item 4, index 3 will be the third choice
-  'Lorem ipsum dolor sit amet, consectetur adipiscing4', # Item 5, index 4 will be the fourth choice
-  'Lorem ipsum dolor sit amet, consectetur adipiscing5'# Item 6, index 5 will be the write statement we need to dsiplay th right stetment if the user enters the wrong choice
-  ,3], # Item 7, index 6 will be the postion of the right answer (ubdex where right answer sits), this will be our check if answer is correct or no
-  2: ["1Lorem ipsum dolor sit amet, consectetur adipiscing?",
-  'Lorem ipsum dolor sit amet, consectetur adipiscing1',
-  'Lorem ipsum dolor sit amet, consectetur adipiscing2',
-  'Lorem ipsum dolor sit amet, consectetur adipiscing3',
-  'Lorem ipsum dolor sit amet, consectetur adipiscing4',
-  'Lorem ipsum dolor sit amet, consectetur adipiscing5'
-  ,1],
-  3: ["2Lorem ipsum dolor sit amet, consectetur adipiscing?",
-  'Lorem ipsum dolor sit amet, consectetur adipiscing1',
-  'Lorem ipsum dolor sit amet, consectetur adipiscing2',
-  'Lorem ipsum dolor sit amet, consectetur adipiscing3',
-  'Lorem ipsum dolor sit amet, consectetur adipiscing4',
-  'Lorem ipsum dolor sit amet, consectetur adipiscing5'
-  ,2],
-  4: ["3Lorem ipsum dolor sit amet, consectetur adipiscing?",
-  'Lorem ipsum dolor sit amet, consectetur adipiscing1',
-  'Lorem ipsum dolor sit amet, consectetur adipiscing2',
-  'Lorem ipsum dolor sit amet, consectetur adipiscing3',
-  'Lorem ipsum dolor sit amet, consectetur adipiscing4',
-  'Lorem ipsum dolor sit amet, consectetur adipiscing5'
-  ,2],
-  5: ["4Lorem ipsum dolor sit amet, consectetur adipiscing?",
-  'Lorem ipsum dolor sit amet, consectetur adipiscing1',
-  'Lorem ipsum dolor sit amet, consectetur adipiscing2',
-  'Lorem ipsum dolor sit amet, consectetur adipiscing3',
-  'Lorem ipsum dolor sit amet, consectetur adipiscing4',
-  'Lorem ipsum dolor sit amet, consectetur adipiscing5'
-  ,5],
-  6: ["5Lorem ipsum dolor sit amet, consectetur adipiscing?",
-  'Lorem ipsum dolor sit amet, consectetur adipiscing1',
-  'Lorem ipsum dolor sit amet, consectetur adipiscing2',
-  'Lorem ipsum dolor sit amet, consectetur adipiscing3',
-  'Lorem ipsum dolor sit amet, consectetur adipiscing4',
-  'Lorem ipsum dolor sit amet, consectetur adipiscing5'
-  ,3],
-  7: ["6Lorem ipsum dolor sit amet, consectetur adipiscing?",
-  'Lorem ipsum dolor sit amet, consectetur adipiscing1',
-  'Lorem ipsum dolor sit amet, consectetur adipiscing2',
-  'Lorem ipsum dolor sit amet, consectetur adipiscing3',
-  'Lorem ipsum dolor sit amet, consectetur adipiscing4',
-  'Lorem ipsum dolor sit amet, consectetur adipiscing5'
-  ,4],
-  8: ["7Lorem ipsum dolor sit amet, consectetur adipiscing?",
-  'Lorem ipsum dolor sit amet, consectetur adipiscing1',
-  'Lorem ipsum dolor sit amet, consectetur adipiscing2',
-  'Lorem ipsum dolor sit amet, consectetur adipiscing3',
-  'Lorem ipsum dolor sit amet, consectetur adipiscing4',
-  'Lorem ipsum dolor sit amet, consectetur adipiscing5'
-  ,1],
-  9: ["8Lorem ipsum dolor sit amet, consectetur adipiscing?",
-  'Lorem ipsum dolor sit amet, consectetur adipiscing1',
-  'Lorem ipsum dolor sit amet, consectetur adipiscing2',
-  'Lorem ipsum dolor sit amet, consectetur adipiscing3',
-  'Lorem ipsum dolor sit amet, consectetur adipiscing4',
-  'Lorem ipsum dolor sit amet, consectetur adipiscing5'
-  ,3],
-  10:["9Lorem ipsum dolor sit amet, consectetur adipiscing?",
-  'Lorem ipsum dolor sit amet, consectetur adipiscing1',
-  'Lorem ipsum dolor sit amet, consectetur adipiscing2',
-  'Lorem ipsum dolor sit amet, consectetur adipiscing3',
-  'Lorem ipsum dolor sit amet, consectetur adipiscing4',
-  'Lorem ipsum dolor sit amet, consectetur adipiscing5'
-  ,5],
-}
 
 def  randomiser():
   global qnum
@@ -101,7 +26,7 @@ class QuizStarter:
         self.quiz_frame.grid()#
                
         #widgets goes below
-        self.heading_label = Label(self.quiz_frame, text="Health Survey", font = ("Helvetica","18","bold"),bg="DarkSeaGreen2")
+        self.heading_label = Label(self.quiz_frame, text="MRGS School Map", font = ("Helvetica","18","bold"),bg="DarkSeaGreen2")
         self.heading_label.grid(row = 0, padx = 20) 
         self.var1 = IntVar() #holds value of radio buttons
         
@@ -146,7 +71,80 @@ class QuizStarter:
 
 class Quiz:    
   def __init__(self, parent):#constructor, The __init__() function is called automatically every time the class is being used to create a new object.
- 
+        #Dictionary has key of number (for each question number) and : the value for each is a list that has 7 items, so index 0 to 6
+        self.questions_answers = {
+          1: ["What is the name of the hall?", #item 1, index 0 will be the question
+          'Watson Hall', # Item 2, index 1 will be the first choice
+          'Ball Hall', # Item 3, index 2 will be the second choice
+          'Butler Hall', # Item 4, index 3 will be the third choice
+          'School Hall', # Item 5, index 4 will be the fourth choice
+          'Cathedral Hall'# Item 6, index 5 will be the write statement we need to dsiplay the right stetment if the user enters the wrong choice
+          ,3], # Item 7, index 6 will be the postion of the right answer (ubdex where right answer sits), this will be our check if answer is correct or no
+          2: ["1Lorem ipsum dolor sit amet, consectetur adipiscing?",
+          'Lorem ipsum dolor sit amet, consectetur adipiscing1',
+          'Lorem ipsum dolor sit amet, consectetur adipiscing2',
+          'Lorem ipsum dolor sit amet, consectetur adipiscing3',
+          'Lorem ipsum dolor sit amet, consectetur adipiscing4',
+          'Lorem ipsum dolor sit amet, consectetur adipiscing5'
+          ,1],
+          3: ["2Lorem ipsum dolor sit amet, consectetur adipiscing?",
+          'Lorem ipsum dolor sit amet, consectetur adipiscing1',
+          'Lorem ipsum dolor sit amet, consectetur adipiscing2',
+          'Lorem ipsum dolor sit amet, consectetur adipiscing3',
+          'Lorem ipsum dolor sit amet, consectetur adipiscing4',
+          'Lorem ipsum dolor sit amet, consectetur adipiscing5'
+          ,2],
+          4: ["3Lorem ipsum dolor sit amet, consectetur adipiscing?",
+          'Lorem ipsum dolor sit amet, consectetur adipiscing1',
+          'Lorem ipsum dolor sit amet, consectetur adipiscing2',
+          'Lorem ipsum dolor sit amet, consectetur adipiscing3',
+          'Lorem ipsum dolor sit amet, consectetur adipiscing4',
+          'Lorem ipsum dolor sit amet, consectetur adipiscing5'
+          ,2],
+          5: ["4Lorem ipsum dolor sit amet, consectetur adipiscing?",
+          'Lorem ipsum dolor sit amet, consectetur adipiscing1',
+          'Lorem ipsum dolor sit amet, consectetur adipiscing2',
+          'Lorem ipsum dolor sit amet, consectetur adipiscing3',
+          'Lorem ipsum dolor sit amet, consectetur adipiscing4',
+          'Lorem ipsum dolor sit amet, consectetur adipiscing5'
+          ,5],
+          6: ["5Lorem ipsum dolor sit amet, consectetur adipiscing?",
+          'Lorem ipsum dolor sit amet, consectetur adipiscing1',
+          'Lorem ipsum dolor sit amet, consectetur adipiscing2',
+          'Lorem ipsum dolor sit amet, consectetur adipiscing3',
+          'Lorem ipsum dolor sit amet, consectetur adipiscing4',
+          'Lorem ipsum dolor sit amet, consectetur adipiscing5'
+          ,3],
+          7: ["6Lorem ipsum dolor sit amet, consectetur adipiscing?",
+          'Lorem ipsum dolor sit amet, consectetur adipiscing1',
+          'Lorem ipsum dolor sit amet, consectetur adipiscing2',
+          'Lorem ipsum dolor sit amet, consectetur adipiscing3',
+          'Lorem ipsum dolor sit amet, consectetur adipiscing4',
+          'Lorem ipsum dolor sit amet, consectetur adipiscing5'
+          ,4],
+          8: ["7Lorem ipsum dolor sit amet, consectetur adipiscing?",
+          'Lorem ipsum dolor sit amet, consectetur adipiscing1',
+          'Lorem ipsum dolor sit amet, consectetur adipiscing2',
+          'Lorem ipsum dolor sit amet, consectetur adipiscing3',
+          'Lorem ipsum dolor sit amet, consectetur adipiscing4',
+          'Lorem ipsum dolor sit amet, consectetur adipiscing5'
+          ,1],
+          9: ["8Lorem ipsum dolor sit amet, consectetur adipiscing?",
+          'Lorem ipsum dolor sit amet, consectetur adipiscing1',
+          'Lorem ipsum dolor sit amet, consectetur adipiscing2',
+          'Lorem ipsum dolor sit amet, consectetur adipiscing3',
+          'Lorem ipsum dolor sit amet, consectetur adipiscing4',
+          'Lorem ipsum dolor sit amet, consectetur adipiscing5'
+          ,3],
+          10:["9Lorem ipsum dolor sit amet, consectetur adipiscing?",
+          'Lorem ipsum dolor sit amet, consectetur adipiscing1',
+          'Lorem ipsum dolor sit amet, consectetur adipiscing2',
+          'Lorem ipsum dolor sit amet, consectetur adipiscing3',
+          'Lorem ipsum dolor sit amet, consectetur adipiscing4',
+          'Lorem ipsum dolor sit amet, consectetur adipiscing5'
+          ,5],
+        }
+
         background_color = "DarkSeaGreen1"#
 
         #frame set up
@@ -156,7 +154,7 @@ class Quiz:
         randomiser() #randomiser will randomly pick a question number which is qnum  
                
         #widgets goes below
-        self.question_label = Label (self.quiz_frame, text = questions_answers[qnum][0], font = ("Helvetica","18","bold"), bg="DarkSeaGreen2")
+        self.question_label = Label (self.quiz_frame, text = self.questions_answers[qnum][0], font = ("Helvetica","18","bold"), bg="DarkSeaGreen2")
         self.question_label.grid(row = 0, padx = 10) 
 
         self.var1 = IntVar() #holds value of radio buttons
@@ -164,28 +162,28 @@ class Quiz:
         #first radio button to hold first choice answer 
         
         #Radiobutton 1
-        self.rb1 = Radiobutton (self.quiz_frame, text = questions_answers[qnum][1], font = ("Helvetica", "14"), bg = background_color, value = 1, variable = self.var1, padx = 10, pady = 10)
+        self.rb1 = Radiobutton (self.quiz_frame, text = self.questions_answers[qnum][1], font = ("Helvetica", "14"), bg = background_color, value = 1, variable = self.var1, padx = 10, pady = 10)
         
         self.rb1.grid(row = 1, sticky = W)
 
         #Radiobutton 2
         
-        self.rb2 = Radiobutton (self.quiz_frame, text = questions_answers[qnum][2], font = ("Helvetica", "14"), bg = background_color, value = 2, variable = self.var1, padx = 10, pady = 10)
+        self.rb2 = Radiobutton (self.quiz_frame, text = self.questions_answers[qnum][2], font = ("Helvetica", "14"), bg = background_color, value = 2, variable = self.var1, padx = 10, pady = 10)
 
         self.rb2.grid (row = 2, sticky = W)
 
         #Radiobutton 3
-        self.rb3 = Radiobutton (self.quiz_frame, text = questions_answers[qnum][3], font = ("Helvetica", "14"), bg = background_color, value = 3, variable = self.var1, padx = 10, pady=10)
+        self.rb3 = Radiobutton (self.quiz_frame, text = self.questions_answers[qnum][3], font = ("Helvetica", "14"), bg = background_color, value = 3, variable = self.var1, padx = 10, pady=10)
 
         self.rb3.grid (row = 3, sticky = W)
 
         #Radiobutton 4
-        self.rb4 = Radiobutton (self.quiz_frame, text = questions_answers[qnum][4], font = ("Helvetica", "14"), bg = background_color, value = 4, variable = self.var1, padx = 10, pady = 10)
+        self.rb4 = Radiobutton (self.quiz_frame, text = self.questions_answers[qnum][4], font = ("Helvetica", "14"), bg = background_color, value = 4, variable = self.var1, padx = 10, pady = 10)
 
         self.rb4.grid (row = 4, sticky = W)
 
         #Radiobutton 5
-        self.rb5 = Radiobutton (self.quiz_frame, text = questions_answers[qnum][5], font = ("Helvetica", "14"), bg = background_color, value = 5, variable = self.var1, padx = 10, pady = 10)
+        self.rb5 = Radiobutton (self.quiz_frame, text = self.questions_answers[qnum][5], font = ("Helvetica", "14"), bg = background_color, value = 5, variable = self.var1, padx = 10, pady = 10)
 
         self.rb5.grid (row = 5, sticky = W)
 
@@ -210,12 +208,12 @@ class Quiz:
   def questions_setup (self):
     randomiser()
     self.var1.set(0)
-    self.question_label.config(text = questions_answers[qnum][0])
-    self.rb1.config(text = questions_answers[qnum][1])
-    self.rb2.config(text = questions_answers[qnum][2])
-    self.rb3.config(text = questions_answers[qnum][3])
-    self.rb4.config(text = questions_answers[qnum][4])
-    self.rb5.config(text = questions_answers[qnum][5])
+    self.question_label.config(text = self.questions_answers[qnum][0])
+    self.rb1.config(text = self.questions_answers[qnum][1])
+    self.rb2.config(text = self.questions_answers[qnum][2])
+    self.rb3.config(text = self.questions_answers[qnum][3])
+    self.rb4.config(text = self.questions_answers[qnum][4])
+    self.rb5.config(text = self.questions_answers[qnum][5])
 
   #This is the method that would get invoked with confrim answer button is clicked, to take care of test_progress
 
@@ -224,7 +222,7 @@ class Quiz:
     scr_label = self.score_label
     choice = self.var1.get()
     if len(asked)>9: #if the question is last
-      if choice == questions_answers[qnum][6]: #if the last question is the correct answer
+      if choice == self.questions_answers[qnum][6]: #if the last question is the correct answer
         score+=1 
         scr_label.configure(text = score)
         self.confirm_button.config(text="Confirm")
@@ -232,15 +230,15 @@ class Quiz:
       else: #if the last question is the incorrect answer
         print(choice)
         score+=0
-        scr_label.configure(text = " The correct answer was " + questions_answers[qnum][5])
+        scr_label.configure(text = " The correct answer was " + self.questions_answers[qnum][5])
         self.confirm_button.config(text="Confirm")
-        self.end_screen
+        self.end_screen()
     else: #if it is not the last question
       if choice==0: #checks if the user has made a choice
         self.confirm_button.config(text="Try again please, you didnt select anyhting")
         choice=self.var1.get()
       else: #if a choice was made and its not the last question
-        if choice == questions_answers[qnum][6]: #if thier choice is correct
+        if choice == self.questions_answers[qnum][6]: #if thier choice is correct
           score+=1
           scr_label.configure(text = score)
           self.confirm_button.config(text="Confirm")
@@ -248,49 +246,42 @@ class Quiz:
         else: #if the choice was wrong
           print(choice)
           score+=0
-          scr_label.configure(text="The correct answer was: " + questions_answers[qnum][5])
+          scr_label.configure(text="The correct answer was: " + self.questions_answers[qnum][5])
           self.confirm_button.configure(text="Confirm")
           self.questions_setup() 
 
 
   def end_screen(self):
-    root.withdraw()
-    name=names[0]
-    file=open("leaderBoard.txt", "a")
+      root.withdraw()
+      name=names[0]
+      file=open("leaderBoard.txt", "a")
 
-    if name == "admin_reset":
-      file=open("leaderBoard.txt", "W")
-
-    else:
       file.write(str(score))
       file.write(" - ")
       file.write(name+"\n")
       file.close()
 
-    inputFile= open("leaderBoard.txt", "r")
-    lineList = inputFile.readlines()
-    lineList.sort()
-    top=[]
-    top5=(lineList[-5:])
+      inputFile= open("leaderBoard.txt", "r")
+      lineList = inputFile.readlines()
+      lineList.sort()
+      top=[]
+      top5=(lineList[-5:])
 
-    for line in top5:
-      point=line.split(" - ")
-      top.append((int(point[0]), point[1]))
-    file.close()
-    top.sort()
-    top.reverse()
-    
-    return_string = ""
-    for i in range (len(top)):
-      return_string +="{} - {}\n".format(top[i][0], top[i][1])
-    print(return_string)
+      for line in top5:
+          point=line.split(" - ")
+          top.append((int(point[0]), point[1]))
+      file.close()
+      top.sort()
+      top.reverse()
+        
+      return_string = ""
+      for i in range (len(top)):
+          return_string +="{} - {}\n".format(top[i][0], top[i][1])
+      print(return_string)
 
-    open_end_screen=End()
-    open_end_screen.listLabel.config(text=return_string)
+      open_end_screen=End()
+      open_end_screen.listLabel.config(text=return_string)
 
-  def close_end(self):
-    self.end_box.destroy()
-    root.withdraw()
 
 class End:
   def __init__(self):
@@ -311,6 +302,10 @@ class End:
     exit_button.grid(row = 4, pady = 20)
 
    
+  def close_end(self):
+    self.end_box.destroy()
+    root.destroy()
+
 randomiser()
 if __name__ == "__main__":
     root = Tk()
